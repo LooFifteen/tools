d6c4ad46a45c21efffa54dbeea8b38ad2ff1950b
Luis <luis@lu15.dev>
2025-07-09 07:12:18 +0100

    feat: add oci building

--- flake.nix
+++ flake.nix
@@ -7,9 +7,10 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
+  outputs = { nixpkgs, rust-overlay, crane, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -17,16 +18,55 @@
         overlays = [ (import rust-overlay) ];
       };
       rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      # crane
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+      templateFilter = path: type: type == "regular" && builtins.match ".*/src/templates/.*" path != null;
+      templateOrCargo = path: type: (templateFilter path type) || (craneLib.filterCargoSources path type);
+      src = pkgs.lib.cleanSourceWith {
+        src = ./.;
+        filter = templateOrCargo;
+        name = "source";
+      };
+
+      # build inputs
       nativeBuildInputs = with pkgs; [
         pkg-config
         rustToolchain
       ];
       buildInputs = with pkgs; [ openssl ];
+
+      # arguments
+      commonArguments = {
+        inherit src buildInputs nativeBuildInputs;
+      };
+      cargoArtifacts = craneLib.buildDepsOnly commonArguments;
+
+      # binary
+      bin = craneLib.buildPackage (commonArguments // {
+        inherit cargoArtifacts;
+      });
+
+      # docker
+      dockerImage = pkgs.dockerTools.streamLayeredImage {
+        name = "tools";
+        tag = "latest";
+        contents = [ bin ];
+        config = {
+          Cmd = [ "${bin}/bin/tools" ];
+          ExposedPorts = [ "3000/tcp" ];
+        };
+      };
     in
     with pkgs;
     {
+      packages.${system} =
+        {
+          inherit bin dockerImage;
+          default = bin;
+        };
       devShells.${system}.default = mkShell {
-        inherit nativeBuildInputs buildInputs;
+        inputsFrom = [ bin ];
       };
     };
 }
